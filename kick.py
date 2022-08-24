709e0c30fc948f6f4871cd90f9225c9ccd9e5d41
Glenn E. Bailey III <337608+replicant0wnz@users.noreply.github.com>
2022-08-24 14:03:40 -0500

    Converting to class so currently broken

--- kick.py
+++ kick.py
@@ -8,34 +8,91 @@ import sys
 import pyaudio
 import mpv
 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
+class KickDrum:
+    """
+    Simple drum module specifically for kick drums but could be used for any drum pad
+    """
+    
+    def __init__(self, config_file="config.yaml"):
+        """
+        Keywords:
+            config_file (str): Full path to config
+        """
 
-for i in range(0, numdevices):
-    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        self.config_file = config_file
+    
+    def _read_config(self):
+        """
+        Reads the yaml config file
 
-stream = p.open(format=pyaudio.paInt16,
-                input_device_index=6,
-                channels=2,
-                rate=44100,
-                input=True,
-                frames_per_buffer=800)
+        Returns:
+            config (dict)
+        """
 
-def rms( data ):
-    count = len(data)/2
-    format = "%dh"%(count)
-    shorts = struct.unpack( format, data )
-    sum_squares = 0.0
-    for sample in shorts:
-        n = sample * (1.0/32768)
-        sum_squares += n*n
-#    return math.sqrt( sum_squares / count )
-    return round(sum_squares)
+        config = {}
 
-player = mpv.MPV(ytdl=True)
+        try:
+            f = open(self.config_file)
+        except ClientError as e:
+            raise Exception(e.response["Error"]["Message"])
 
+        config_yaml = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+        return config_yaml
+
+
+    def _open_device(self):
+        """
+        Creates a pyaudio interface object
+        """
+         interface = pyaudio.PyAudio()
+
+        return interface
+
+    def _open_stream(self, input_device_index=None, channels=2, rate=44100):
+        """
+        Open a stream on the sepcific device
+        """
+        stream = p.open(format=pyaudio.paInt16,
+                        input_device_index=input_device_index,
+                        channels=channels,
+                        rate=rate,
+                        input=True,
+                        frames_per_buffer=800)
+
+    def _rms(self, data):
+        """
+        Convert stream data into numberical format
+        """
+        count = len(data)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, data )
+        sum_squares = 0.0
+
+        for sample in shorts:
+            n = sample * (1.0/32768)
+            sum_squares += n*n
+
+        return round(sum_squares)
+
+    def _player(self):
+        """
+        Use mpv to output sound
+        """
+        player = mpv.MPV()
+        
+        return player
+
+    def list_devices(self, interface=None):
+        """
+        List available input devices
+        """
+        info = interface.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+
+        for i in range(0, numdevices):
+            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 while True:
     data = stream.read(1024, exception_on_overflow=False)
