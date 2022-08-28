df1f621796676b235f45fd379b2998429cad6ae4
Glenn E. Bailey III <337608+replicant0wnz@users.noreply.github.com>
2022-08-27 22:34:25 -0500

    Updates and ran black

--- kick.py
+++ kick.py
@@ -7,12 +7,16 @@ import sys
 
 import pyaudio
 import mpv
+import yaml
+
 
 class KickDrum:
     """
-    Simple drum module specifically for kick drums but could be used for any drum pad
+    Simple non-velcotiy drum module specifically for kick drums but could be
+    used for any drum pad
+
     """
-    
+
     def __init__(self, config_file="config.yaml"):
         """
         Keywords:
@@ -20,7 +24,7 @@ class KickDrum:
         """
 
         self.config_file = config_file
-    
+
     def _read_config(self):
         """
         Reads the yaml config file
@@ -29,49 +33,52 @@ class KickDrum:
             config (dict)
         """
 
-        config = {}
-
         try:
             f = open(self.config_file)
-        except ClientError as e:
-            raise Exception(e.response["Error"]["Message"])
+        except FileNotFoundError as e:
+            print(e)
 
         config_yaml = yaml.load(f.read(), Loader=yaml.FullLoader)
 
         return config_yaml
 
-
     def _open_device(self):
         """
         Creates a pyaudio interface object
         """
-         interface = pyaudio.PyAudio()
+        interface = pyaudio.PyAudio()
 
         return interface
 
-    def _open_stream(self, input_device_index=None, channels=2, rate=44100):
+    def _open_stream(
+        self, interface=None, input_device_index=None, channels=None, rate=None
+    ):
         """
         Open a stream on the sepcific device
         """
-        stream = p.open(format=pyaudio.paInt16,
-                        input_device_index=input_device_index,
-                        channels=channels,
-                        rate=rate,
-                        input=True,
-                        frames_per_buffer=800)
+        stream = interface.open(
+            format=pyaudio.paInt16,
+            input_device_index=input_device_index,
+            channels=channels,
+            rate=rate,
+            input=True,
+            frames_per_buffer=800,
+        )
+
+        return stream
 
     def _rms(self, data):
         """
         Convert stream data into numberical format
         """
-        count = len(data)/2
-        format = "%dh"%(count)
-        shorts = struct.unpack( format, data )
+        count = len(data) / 2
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, data)
         sum_squares = 0.0
 
         for sample in shorts:
-            n = sample * (1.0/32768)
-            sum_squares += n*n
+            n = sample * (1.0 / 32768)
+            sum_squares += n * n
 
         return round(sum_squares)
 
@@ -80,7 +87,7 @@ class KickDrum:
         Use mpv to output sound
         """
         player = mpv.MPV()
-        
+
         return player
 
     def list_devices(self, interface=None):
@@ -88,22 +95,41 @@ class KickDrum:
         List available input devices
         """
         info = interface.get_host_api_info_by_index(0)
-        numdevices = info.get('deviceCount')
+        numdevices = info.get("deviceCount")
 
         for i in range(0, numdevices):
-            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+            if (
+                interface.get_device_info_by_host_api_device_index(0, i).get(
+                    "maxInputChannels"
+                )
+            ) > 0:
+                print(
+                    "Input Device id ",
+                    i,
+                    " - ",
+                    interface.get_device_info_by_host_api_device_index(0, i).get(
+                        "name"
+                    ),
+                )
 
-    def start_controller(self):
+    def start_controller(self, stream=None):
         """
         Start the loop and listen for events
         """
 
-        config = self._read_config()
+        config = self._read_config()["config"]
+        interface = self._open_device()
+        stream = self._open_stream(
+            interface=interface,
+            input_device_index=config["device_id"],
+            channels=config["channels"],
+            rate=config["rate"],
+        )
+        player = self._player()
 
         while True:
             data = stream.read(1024, exception_on_overflow=False)
-            x = rms(data)
-            if x > 0:
+            x = self._rms(data)
+            if x > config["threshold"]:
                 print(x)
-                player.play('bass.wav')
+                player.play(config["sample"])
